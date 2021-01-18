ee370892bf67c2a9b384de428495e5aa9a9d8c5f
Riccardo Caprai <bisark@gmail.com>
2021-01-18 17:41:45 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import SVGElement from "./SVGElement";
+
+export default function App() {
+  return (
+    <div className="App">
+      <SVGElement to="#fcba03" from="#000000" />
+    </div>
+  );
+}
